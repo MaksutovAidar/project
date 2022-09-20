b6b403c976e93f7dc62619ca9243354852842a84
Maksutov Aidar <MaksutovAidarZ@mail.ru>
2022-09-20 21:11:58 +0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+"use.sctrict";
+
+// alert('Приветствую!')
+// const result = confirm('Вы хотите остаться на этом сайте?');
+// console.log(result);
+// const answer = +prompt('Вам есть 18?', 'Да');
+// console.log(answer + 77);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Вас зовут?', '');
+// answers[1] = prompt('Сколько Вам лет?', '');
+// answers[2] = prompt('С какого Вы города?', '');
+
+// document.write(answers);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}`);
+
+const user = 'Иван';
+alert(`Добро пожаловать, ${user}`);
